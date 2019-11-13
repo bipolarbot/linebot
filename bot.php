c0df1e7724bc211906bb426c602155c9553c0ccb
bipolarbot <57701061+bipolarbot@users.noreply.github.com>
2019-11-13 15:18:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'LUY9MGaOSmrnBwn2UpZaKvmHDg89+vwbos8FFmnyC3olZsS+2z/fwAsafchAQfSoF+8BXszRa2I0K8W5zFxeHJPaxO74swjn8DQyRxOvOFiyRdxa7xjvnjPixSa4sB+47Ht2wF54VA/5vrNqUoTLkwdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = 'fd062a3c11fd035323dd6a4cd52b8e0a';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
